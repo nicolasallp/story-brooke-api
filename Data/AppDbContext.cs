757c64d75cfb83ff1da2d7b765780085b145e708
Nicolas <nikolasalp@gmail.com>
2025-11-05 16:10:20 -0300

    add: data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+using story_brook_api.Models;
+
+namespace story_brook_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Wishlist> Wishlist { get; set; }
+    }
+}
